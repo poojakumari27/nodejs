3892a11c273f7e0becec78038f4cc108c7c1535a
poojakumari27 <poojakumari27ee@gmail.com>
2025-01-07 14:47:37 +0530

    basics

---
+++ basic.js/o5_stings.js
@@ -0,0 +1,13 @@
+const name ="     pooja"
+var age=18
+const repocount=5
+console.log(`my name is ${name}  and my rep count is ${repocount}`);
+const nameb = new String("pooja")
+console.log(nameb[0])
+
+console.log(name.length)
+console.log(name.tolowercase)
+console.log(name.substring(1,4))//4 index is not included
+onsole.log(name.trim())//remove the spaces
+
+
--- o3_conversion.js
+++
@@ -1,4 +0,0 @@
-"use strict";//treat all js  code as new version
-
-let score=33
-console.log([typeof score])
