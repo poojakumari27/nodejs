41d8fe52b38ec4cc0a159cd15f6a1be2dca83db7
poojakumari27 <poojakumari27ee@gmail.com>
2025-01-15 14:26:30 +0530

    basic

---
+++ o5_dom/javascript.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+// alert("wait");
+// console.dir(window)
+// console.dir(document)
+// console.dir(document.body)
+// console.log(document.body)
+// let heading =document.getElementById("heading")
+// console.log(heading)
+// let heading1=document.getElementsByClassName("heading")
+// console.log(heading1)
+// let firstEl=document.querySelector("p");
+// console.dir(firstEl);
+// let div=document.querySelector("div")
+// console.dir(div)
+//  div=document.querySelector("div")
+// console.dir(div)
+// let val=div.getAttribute("id")
+// console.dir(val)
+
+// let val1=div.setAttribute("id","class")
+// console.log(val1)
+
+// let div1=document.querySelector("div")
+// div1.style.backgroundColor="red"
+// let newbutton= document.createElement("button")
+// newbutton.innerText="click mw"
+// console.log(newbutton)
+// div1.append(newbutton)
+let newpara = document.createElement("p");
+newClass.innerText = "Hi, I am";
+document.querySelector("body").appendChild(newpara);
