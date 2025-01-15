21a46acf478618679bef9ec504b9691bdad9e885
poojakumari27 <poojakumari27ee@gmail.com>
2025-01-15 14:26:24 +0530

    basic

--- basic.js/o1_variable.js
+++
@@ -1,9 +0,0 @@
-const accountId =144553 // constant value we cant change
-let accoundEmail="pooja@gmail.accoundEmail" //we can change these values
-var accountPassword="123456"
-
-accountcity= "jaipur"// possibility of declaring like this
-//accountId=2
-console.log(accountId)
-console.table([accountId,accountPassword,accoundEmail])
-/* we dont prefer using var instead of that we use let  bcouase of issue in  block scope {}and function scope  */
